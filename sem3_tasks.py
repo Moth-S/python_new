2317484a8e5b04bc5b852b05197c9d4969d7353a
Lyubov Si <gse182@yandex.ru>
2023-02-02 23:04:55 +0300

    add solution task 2 from seminar 3

--- sem3_tasks.py
+++ sem3_tasks.py
@@ -2,20 +2,45 @@
 # стоящих на нечётной позиции.
 # Пример: [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 
+# from random import randint
+
+# length_list = int(input('Length list: '))
+# lst = []
+# summa = 0
+
+# for i in range(length_list):
+#     lst.append(randint(1,10))
+
+# for j in range(len(lst)):
+#     if j % 2 == 1:
+#         summa += lst[j]
+
+# print(f"{lst} -> answer {summa}")
+
+# ________________________________________________________________________________________________________________________
+
+# Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+# Пример:
+# - [2, 3, 4, 5, 6] => [12, 15, 16];
+# - [2, 3, 5, 6] => [12, 15]
+
 from random import randint
 
-length_list = int(input('Length list: '))
-lst = []
-summa = 0
+lst, lst_sum_pairs = [], []
+length_list = int(input("Length list: "))
 
-for i in range(length_list):
-    lst.append(randint(1,10))
+for j in range(length_list):
+    lst.append(randint(1, 10))
 
-for j in range(len(lst)):
-    if j % 2 == 1:
-        summa += lst[j]
+i = 0
+while i < len(lst) // 2 + len(lst) % 2:
+    lst_sum_pairs.append(lst[i] * lst[len(lst) - 1 - i])
+    i += 1
 
-print(f"{lst} -> answer {summa}")
+print(f"{lst} => {lst_sum_pairs}")
+
 
 
 
