b9cd9e027ecd3cb175c96d24cddbf450dd8d2387
Lyubov Si <gse182@yandex.ru>
2023-02-02 23:23:58 +0300

    add solution task 4 from seminar 3

--- sem3_tasks.py
+++ sem3_tasks.py
@@ -43,34 +43,46 @@
 
 # ________________________________________________________________________________________________________________________
 
-# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным 
+# Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным
 # и минимальным значением дробной части элементов.
 
 # Пример:
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-from random import uniform
+# from random import uniform
 
-float_lst = []
+# float_lst = []
 
-for i in range(6):
-    float_lst.append(round(uniform(0, 20.5), 2))
+# for i in range(6):
+#     float_lst.append(round(uniform(0, 20.5), 2))
 
-max_remainder = 0
-min_remainder = 0.999
+# max_remainder = 0
+# min_remainder = 0.999
 
-for j in float_lst:
+# for j in float_lst:
 
-    if j - int(j) > max_remainder:
-        max_remainder = j - int(j)
+#     if j - int(j) > max_remainder:
+#         max_remainder = j - int(j)
 
-    if j - int(j) < min_remainder:
-        min_remainder = j - int(j)
+#     if j - int(j) < min_remainder:
+#         min_remainder = j - int(j)
 
-print(f"{float_lst} => {round(max_remainder-min_remainder,2)}")
+# print(f"{float_lst} => {round(max_remainder-min_remainder,2)}")
 
+# ________________________________________________________________________________________________________________________
 
+# Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 
+# Пример:
+# - 45 -> 101101
+# - 3 -> 11
+# - 2 -> 10
 
+num = int(input())
+strng = ''
 
+while num > 0:
+    strng += str(num % 2)
+    num //= 2
 
+print(strng[::-1])
