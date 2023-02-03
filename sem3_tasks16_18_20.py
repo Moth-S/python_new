dee632c36a7952bcacb1991ebec8d6f314edca8d
Lyubov Si <gse182@yandex.ru>
2023-02-03 22:53:05 +0300

    add solution task 20 from seminar 3

--- sem3_tasks16_18_20.py
+++ sem3_tasks16_18_20.py
@@ -33,24 +33,71 @@
 # 6
 # -> 5
 
-from random import randint
+# from random import randint
 
-length_list = int(input('Length list: '))
-lst = []
-difference = 11
+# length_list = int(input('Length list: '))
+# lst = []
+# difference = 11
 
-for i in range(length_list):
-    lst.append(randint(1, 10))
+# for i in range(length_list):
+#     lst.append(randint(1, 10))
 
-print(lst)
+# print(lst)
 
-num = int(input('number: '))
+# num = int(input('number: '))
 
-for j in lst:
-    if abs(j-num) < difference:
-        difference = abs(j-num)
-        near_num = j
+# for j in lst:
+#     if abs(j-num) < difference:
+#         difference = abs(j-num)
+#         near_num = j
 
-print(near_num)
+# print(near_num)
 
+# _____________________________________________________________________________________________
 
+# Задача 20: В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
+# В случае с английским алфавитом очки распределяются так:
+# A, E, I, O, U, L, N, S, T, R – 1 очко;
+# D, G – 2 очка;
+# B, C, M, P – 3 очка;
+# F, H, V, W, Y – 4 очка;
+# K – 5 очков;
+# J, X – 8 очков;
+# Q, Z – 10 очков.
+# А русские буквы оцениваются так:
+# А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+# Д, К, Л, М, П, У – 2 очка;
+# Б, Г, Ё, Ь, Я – 3 очка;
+# Й, Ы – 4 очка;
+# Ж, З, Х, Ц, Ч – 5 очков;
+# Ш, Э, Ю – 8 очков;
+# Ф, Щ, Ъ – 10 очков.
+# Напишите программу, которая вычисляет стоимость введенного пользователем слова.
+# Будем считать, что на вход подается только одно слово, которое содержит либо только английские,
+# либо только русские буквы.
+
+# пример
+
+# Ввод:
+# ноутбук
+# Вывод:
+# 12
+
+price_eng_alphabet = {'aeioulnstr': 1, 'dg': 2,
+                      'bcmp': 3, 'fhvwy': 4, 'k': 5, 'jx': 8, 'qz': 10}
+price_rus_alphabet = {'авеинорст': 1, 'дклмпу': 2,
+                      'бгёья': 3, 'йы': 4, 'жзхцч': 5, 'шэю': 8, 'фщъ': 10}
+word = input('word: ')
+points = 0
+
+for j in word:
+    
+    for e in price_eng_alphabet:
+        if j in e:
+            points += price_eng_alphabet[e]
+
+    for r in price_rus_alphabet:
+        if j in r:
+            points += price_rus_alphabet[r]
+
+print(points)
