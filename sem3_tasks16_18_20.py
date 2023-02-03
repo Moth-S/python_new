f851185c035afb28ba8ed611a91ce1ca559b020c
Lyubov Si <gse182@yandex.ru>
2023-02-03 22:52:18 +0300

    add solution task 18 from seminar 3

--- sem3_tasks16_18_20.py
+++ sem3_tasks16_18_20.py
@@ -8,16 +8,49 @@
 # 3
 # -> 1
 
+# from random import randint
+
+# length_list = int(input('Length list: '))
+# lst = []
+
+# for i in range(length_list):
+#     lst.append(randint(1, 10))
+
+# print(lst)
+
+# desired_value = int(input('value search: '))
+# print(f"-> {lst.count(desired_value)}")
+
+# _____________________________________________________________________________________________
+
+# Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+# пример
+# 5
+# 1 2 3 4 5
+# 6
+# -> 5
+
 from random import randint
 
 length_list = int(input('Length list: '))
 lst = []
+difference = 11
 
 for i in range(length_list):
     lst.append(randint(1, 10))
 
 print(lst)
 
-desired_value = int(input('value search: '))
-print(f"-> {lst.count(desired_value)}")
+num = int(input('number: '))
+
+for j in lst:
+    if abs(j-num) < difference:
+        difference = abs(j-num)
+        near_num = j
+
+print(near_num)
+
 
