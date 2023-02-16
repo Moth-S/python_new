07520274220165e2d13dac764ba9ead36b46887a
LyubovSi <65710849+Moth-S@users.noreply.github.com>
2023-02-16 23:47:53 +0300

    add solution task 32

--- sem6_tasks30_32.py
+++ sem6_tasks30_32.py
@@ -3,12 +3,34 @@
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
 
-first_number = int(input('first number: '))
-diff = int(input('difference: '))
-amount = int(input('amount of numbers: '))
+# first_number = int(input('first number: '))
+# diff = int(input('difference: '))
+# amount = int(input('amount of numbers: '))
+# lst = []
+
+# while amount > 0:
+#     lst.append(first_number+(amount-1)*diff)
+#     amount -= 1
+# print(sorted(lst))
+
+# _____________________________________________________________________________________
+
+# Задача 32: Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и
+# не больше заданного максимума)
+
+from random import randint
+
+start_range = int(input("range start value: "))
+end_range = int(input("range end value: "))
+n = int(input('Length list: '))
 lst = []
 
-while amount > 0:
-    lst.append(first_number+(amount-1)*diff)
-    amount -= 1
-print(sorted(lst))
+for i in range(n):
+    lst.append(randint(1, 100))
+
+print(lst)
+
+for j in range(len(lst)):
+    if start_range <= lst[j] <= end_range:
+        print(j, end=' ')
