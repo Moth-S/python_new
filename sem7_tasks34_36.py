c1a14d06eeb1a8e9c2a5da1b96cd9ebecd2a681c
LyubovSi <65710849+Moth-S@users.noreply.github.com>
2023-02-21 23:23:12 +0300

    add solution task 36

--- sem7_tasks34_36.py
+++ sem7_tasks34_36.py
@@ -13,17 +13,50 @@
 # Вывод:
 # Парам пам-пам
 
-poem=input('poem: ')
-vowels=[]
+# poem=input('poem: ')
+# vowels=[]
 
-for row in poem.split():
-    k=0
-    for poem_char in row:
-        if poem_char in 'аеёиоуыуюя':
-            k+=1
-    vowels.append(k)
+# for row in poem.split():
+#     k=0
+#     for poem_char in row:
+#         if poem_char in 'аеёиоуыуюя':
+#             k+=1
+#     vowels.append(k)
 
-if vowels.count(vowels[0])==len(vowels):
-    print('Парам пам-пам')
-else:
-    print('Пам парам')
+# if vowels.count(vowels[0])==len(vowels):
+#     print('Парам пам-пам')
+# else:
+#     print('Пам парам')
+
+# _________________________________________________________________________________________________
+
+# Задача 36:
+#  Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6),
+# которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца.
+# Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны.
+# Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля).
+# Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например,
+# у операции умножения.
+
+# Ввод:
+# print_operation_table(lambda x, y: x * y) Вывод:
+# 1 2 3 4 5 6
+
+# 2 4 6 8 10 12
+# 3 6 9 12 15 18
+# 4 8 12 16 20 24
+# 5 10 15 20 25 30
+# 6 12 18 24 30 36
+
+
+operation = (lambda x, y: x * y)
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for num in range(1, num_rows+1):
+        for num1 in range(1, num_columns+1):
+            print(operation(num, num1), end=' ')
+        print()
+
+
+print_operation_table(operation)
